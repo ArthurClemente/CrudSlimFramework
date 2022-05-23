ed1f88bcb500218fd93a0bea601c8094738e1253
ArthurClemente <arthurclementte@hotmail.com>
2022-05-23 13:49:20 -0300

    Arquivo de exemplo com as variáveis globais utilizadas.

---
+++ env.example.php
@@ -0,0 +1,5 @@
+<?php
+// Arquivo com variáveis globais que podem ser alteradas quando o sistema for para produção
+
+putenv('DISPLAY_ERRORS_DETAILS'. false); // Mostra os detalhes dos erros. Quando for para produção display erros vira false.
+
