7a29eda9fc36929b4578578e8966b5f1fa06052f
ArthurClemente <arthurclementte@hotmail.com>
2022-05-26 11:52:08 -0300

    Adicionados os exemplos de variáveis para conexão com o banco de dados.

--- env.example.php
+++ env.example.php
@@ -3,3 +3,7 @@
 
 putenv('DISPLAY_ERRORS_DETAILS'. false); // Mostra os detalhes dos erros. Quando for para produção display erros vira false.
 
+putenv('BANCO_SQLSVR_SERVER='); // Servidor/Host
+putenv('BANCO_SQLSVR_DATABASE='); // Nome do banco
+putenv('BANCO_SQLSVR_USER='); // Usuário do banco
+putenv('BANCO_SQLSVR_PASSWORD='); // Senha do banco
