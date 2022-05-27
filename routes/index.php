0ee6b8e3cb67a10e45b4af0f280a901947d8ee80
ArthurClemente <arthurclementte@hotmail.com>
2022-05-27 13:55:24 -0300

    Criadas as rotas para as requisições get, post, put e delete.

--- routes/index.php
+++ routes/index.php
@@ -7,7 +7,11 @@ use function src\slimConfiguration;
 $app = new \Slim\App(slimConfiguration());
 
 // #================================================
-$app->get('/', UserController::class . ':getUsers'); // "::class" retorna uma string com o caminho completo(namespace e a classe)
+// "::class" retorna uma string com o caminho completo(namespace e a classe)
+$app->get('/user', UserController::class . ':getUsers'); // busca os dados
+$app->post('/user', UserController::class . ':insertUsers'); // insere os dados
+$app->put('/user', UserController::class . ':updateUsers'); // altera os dados
+$app->delete('/user', UserController::class . ':deleteUsers'); // deleta o usuário
 // #================================================
 
 $app->run();
