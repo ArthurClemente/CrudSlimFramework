d6a6bb9920ba76978039bf32d38e7e68dcabf481
ArthurClemente <arthurclementte@hotmail.com>
2022-05-31 08:56:07 -0300

    Alterado o retorno de getAllUsers e implementada a função insertUsers que insere as informações no banco de dados.

--- App/DAO/UserDAO.php
+++ App/DAO/UserDAO.php
@@ -3,6 +3,7 @@
 namespace App\DAO;
 
 use App\DAO\Conexao;
+use App\Models\UserModel;
 
 class UserDAO extends Conexao
 {
@@ -11,7 +12,7 @@ class UserDAO extends Conexao
         parent::__construct(); // Chama o __construct de Conexao, iniciando uma conexão com o banco de dados
     }
 
-    public function getAllUsers() 
+    public function getAllUsers(): array 
     {
         $pdo = $this->pdo;
 
@@ -27,10 +28,21 @@ class UserDAO extends Conexao
 
         $users = $query->fetchAll(\PDO::FETCH_ASSOC);
 
-        echo "<pre>";
-        foreach($users as $user)
-        {
-            print_r($user);
-        }
+        return $users;
+    }
+
+    public function insertUser(UserModel $user): void
+    {
+        $sql = "INSERT INTO CrudArtu
+                VALUES(
+                    :id,
+                    :username,
+                    :pass);";
+        $pdo = $this->pdo
+        ->prepare($sql);
+        $pdo->bindValue(':id', $user->getId(), \PDO::PARAM_INT);
+        $pdo->bindValue(':username', $user->getName(), \PDO::PARAM_STR);
+        $pdo->bindValue(':pass', $user->getPassWord(), \PDO::PARAM_STR);
+        $pdo->execute();
     }
 }
