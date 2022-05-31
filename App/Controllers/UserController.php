2e7f26c589d1f58ca7fd208c99d7d58e16a340a2
ArthurClemente <arthurclementte@hotmail.com>
2022-05-31 08:54:23 -0300

    Alterado o response de getUsers e implementada a função insertUsers.

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\DAO\UserDAO;
+use App\Models\UserModel;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -14,19 +15,31 @@ final class UserController
     {
         
         $userDAO = new UserDAO();
-        $userDAO->getAllUsers();
-
+        $users = $userDAO->getAllUsers();
+        $response = $response->withJson($users);
 
         return $response;
     }
+    
     public function insertUsers(Request $request, Response $response, array $args): Response
     {
-        $response = $response->withJson([
-            'message' => 'new world'
-        ]);
 
+        $data = $request->getParsedBody();
+
+        $userDAO = new UserDAO();
+        $user = new UserModel(); // instancia o model de usuário
+        $user->setId($data['id']) // Insere as variávies passadas no corpo da requisição "json" nas variáveis de UserModel.
+            ->setName($data['username'])
+            ->setPassWord($data['pass']);
+
+        $userDAO->insertUser($user);
+
+        $response = $response->withJson([
+            'message' => 'Usuário inserido com sucesso!!'
+        ]);
         return $response;
     }
+    
     public function updateUsers(Request $request, Response $response, array $args): Response
     {
         $response = $response->withJson([
