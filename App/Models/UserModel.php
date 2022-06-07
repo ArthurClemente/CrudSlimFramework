aa4c6478f6e747a75e9c3e90a18f44091aa196e4
ArthurClemente <arthurclementte@hotmail.com>
2022-06-07 10:39:36 -0300

    Excluidos o getter e o setter do Id do usuário.

--- App/Models/UserModel.php
+++ App/Models/UserModel.php
@@ -5,10 +5,6 @@ namespace App\Models;
 final class UserModel
 {
     /**
-     * @var int
-     */
-    private $id;
-    /**
      * @var string
      */
     private $name;
@@ -17,25 +13,12 @@ final class UserModel
      */
     private $passWord;
 
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-    public function setId($id): UserModel // Ao retornar o próprio model, é possível acessar os métodos de forma encadeada.
-    {   // Ex:. $user = new UserModel | $user->setId()->setName()->setPassWord().
-        $this->id = $id;
-        return $this;
-    }
-
     public function getName(): string
     {
         return $this->name;
     }
-    public function setName(string $name): UserModel
-    {
+    public function setName(string $name): UserModel // Ao retornar o próprio model, é possível acessar os métodos de forma encadeada.
+    {// Ex:. $user = new UserModel | $user->setName()->setPassWord().
         $this->name = $name;
         return $this;
     }
